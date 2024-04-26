5d3ce2dd02d37469cf77408a84afadf0d82063c4
Bashir Amokomowo <barsheyr@gmail.com>
2024-04-27 00:04:08 +0100

    Navfix

--- components/Navlinks.tsx
+++ components/Navlinks.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navlinks = () => {
   return (
-    <div className="flex flex-col z-20 py-3 gap-5">
+    <div className="flex lg:flex-row flex-col z-20 py-3 gap-5">
       <Link href="/" className="flex lg:hidden">
         Home
       </Link>
